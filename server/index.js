6ef4a0254c63e041f5cce1aff68ec2aaebb73e8f
Sanja Salihovic <salihovicsanja5@gmail.com>
2023-07-13 14:41:31 +0200

    created get route for fetching all repositories from the last week, created post route for making repository favourite

--- server/index.js
+++ server/index.js
@@ -4,8 +4,32 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const { Octokit } = require('octokit');
+
+const octokit = new Octokit({
+  auth: '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'
+})
+
+// GET route for getting repositories
+app.get('/repositories', async (req, res) => {
+  try {
+    const response = await octokit.request('GET /search/repositories', {
+      q: 'topics'
+    });
+    const repositories = response.data;
+    res.json(repositories);
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+    res.status(500).json({ error: 'Failed to fetch repositories' });
+  }
+})
+
+// POST route for favoriting a repository
+app.post('/favorite', (req, res) => {
+  let favoriteRepositories = [];
+  const { repository } = req.body;
+  favoriteRepositories.push(repository);
+  res.status(201).json({ message: 'Repository favorited successfully' });
 });
 
 app.listen(PORT, () => {
